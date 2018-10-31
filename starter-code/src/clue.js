8225def13bf83182eb1d842179288354063539f9
Omar Zagal <omarzaz12@gmail.com>
2018-10-31 13:24:45 -0600

    Se termina ejercicio de Clue

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -84,11 +84,137 @@ name: Guest House
 name: Patio
 */
 
-// Characters Collection
-var charactersArray = [];
+// Characters Collection//
+var characters = [
+    {
+        first_name: "Jacob",
+        last_name: "Green",
+        color: "green",
+        description: "He has a lot of connections",
+        age: 45,
+        image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        occupation: "Entrepreneur"
+    },
+
+    {
+        first_name: "Doctor",
+        last_name: "Orchid",
+        color: "white",
+        description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        age: 26,
+        image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation: "Scientist"
+    },
+
+    {
+        first_name: "Victor",
+        last_name: "Plum",
+        color: "purple",
+        description: "Billionare video game designer",
+        age: 22,
+        image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation: "Designer"
+    },
+
+    {
+        first_name: "Kasandra",
+        last_name: "Scarlet",
+        color: "red",
+        description: "She is an A-list movie star with a dark past",
+        age: 31,
+        image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation: "Actor,"
+    },
+
+    {
+        first_name: "Eleanor",
+        last_name: "Peacock",
+        color: "blue",
+        description: "She is from a wealthy family and uses her status and money to earn popularity",
+        age: 36,
+        image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation: "Socialité",
+    },
+
+    {
+        first_name: "Jack",
+        last_name: "Mustard",
+        color: "yellow",
+        description: "He is a former football player who tries to get by on his former glory",
+        age: 62,
+        image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        occupation: "Retired Football player",
+    }
+];
+
+
+// Weapons Collection//
+var weapons = [
+    {
+        name: "rope",
+        weight: 10
+    },
+
+    {
+        name: "knife",
+        weight: 8
+    },
+
+    {
+        name: "candlestick",
+        weight: 2
+    },
+
+    {
+        name: "dumbbell",
+        weight: 30
+    },
+
+    {
+        name: "poison",
+        weight: 2
+    },
+
+    {
+        name: "axe",
+        weight: 15
+    },
+
+    {
+        name: "bat",
+        weight: 13
+    },
+
+    {
+        name: "trophy",
+        weight: 25
+    },
+
+    {
+        name: "pistol",
+        weight: 20
+    }
+];
 
 // Rooms' Collection
-var roomsArray = [];
+var room = ["Dinning Room", "Conservatory", "Kitchen", "Study", "Library", "Billiard Room", "Lounge", "Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater", "Guest House", "Patio",]
 
-// Weapons Collection
-var weaponsArray = [];
+//numeros aleatorios//
+var c = Math.floor((Math.random() * 6))
+
+var w = Math.floor((Math.random() * 9))
+
+var r = Math.floor((Math.random() * 15))
+
+
+//Tarjeta finake//
+
+var misteryRoom = room[r];
+
+var misteryWeapons = weapons[w];
+
+var misteryCharacter = characters[c];
+
+//impresion de la tarjeta//
+
+console.log(characters[c].first_name + " " + characters[c].last_name + " killed Mr.Boddy using the " + weapons[w].name + " in the " + misteryRoom);
